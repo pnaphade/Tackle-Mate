5e548cbcb7ba559785663581699c55a7d1adfd97
Priya Naphade <priyanaphade31@gmail.com>
2022-10-05 00:12:51 -0400

    extracting body landmarks

--- pose-reconstruct.py
+++ pose-reconstruct.py
@@ -3,18 +3,22 @@
 import cv2
 import mediapipe as mp
 import sys
+import numpy as np
+
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
 mp_pose = mp.solutions.pose
 
+FRAME_RATE = 30
+fourcc = cv2.VideoWriter_fourcc('m', 'p', 'g', '4')
 input_vid = sys.argv[1]
 cap = cv2.VideoCapture(input_vid)
-#output_vid = cv2.VideoWriter(input_vid+"_marked", apiPreference = 0, fourcc=0x00000021, fps = 60)
 
-with mp_pose.Pose(
+frame_counter = 0
+landmarks_timeseries = []
 
-    min_detection_confidence=0.5,
-    min_tracking_confidence=0.5) as pose:
+with mp_pose.Pose(min_detection_confidence=0.1,
+        min_tracking_confidence=0.1) as pose: # how to tune these parameters?
 
     while cap.isOpened():
         success, image = cap.read()
@@ -23,11 +27,16 @@ with mp_pose.Pose(
             print("Ignoring empty camera frame.")
             break
 
+        frame_counter += 1
+
         # To improve performance, optionally mark the image as not writeable to
         # pass by reference.
         image.flags.writeable = False
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
         results = pose.process(image)
+
+        if (results.pose_landmarks is not None):
+            landmarks_timeseries.append(results.pose_landmarks.landmark)
 
         # Draw the pose annotation on the image.
         image.flags.writeable = True
@@ -43,8 +52,23 @@ with mp_pose.Pose(
         if cv2.waitKey(5) & 0xFF == 27:
             break
 
-        # Write to output video
-        #output_vid.write(image)
+print("Total number of frames:", frame_counter)
+print("Total number of frames with landmarks calculated:",
+        len(landmarks_timeseries))
+landmarks_50 = landmarks_timeseries[50]
+print("Number of landmarks:", len(landmarks_50))
+
+body_areas = ["left shoulder", "right shoulder", "left elbow",
+                    "right elbow", "left wrist", "right wrist",
+                    "left hip", "right hip", "left knee", "right knee",
+                    "left ankle", "right ankle"]
+body_areas_index = [11, 12, 13, 14, 15, 16, 23, 24, 25, 26, 27, 28]
+
+body_index = dict(zip(body_areas, body_areas_index))
+
+for body, index in body_index.items():
+    print(body)
+    print(landmarks_50[index])
+    print()
 
 cap.release()
-#output_vid.release()
