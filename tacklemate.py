d179c35eb9557b2390c207d524b75c4f2bd5cf60
Priya Naphade <priyanaphade31@gmail.com>
2022-10-26 09:24:54 -0400

    barebones attempt at web app

---
+++ tacklemate.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+import flask
+import psycopg2
+import urllib.parse as up
+
+# database_url = "postgres://
+
+app = flask.Flask(__name__)
+
+@app.route('/', methods=['GET'])
+@app.route('/index', methods=['GET'])
+def index():
+    html_code = flask.render_template('index.html')
+    response = flask.make_response(html_code)
+    return response
+
+@app.errorhandler(404)
+def page_not_found(e):
+    html_code = flask.render_template('404.html')
+    response = flask.make_response(html_code)
+    return response
+
+if __name__ == '__main__':
+    app.run(debug=True)
