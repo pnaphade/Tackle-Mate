a53fd2c0ad4ef05db9ff2c3772c0feec1231a1a0
Priya Naphade <priyanaphade31@gmail.com>
2023-01-08 02:52:05 -0500

    attempting debug

--- tacklemate.py
+++ tacklemate.py
@@ -35,7 +35,7 @@ def logoutgoogle():
 # For local development only. MoveNet is accessed via a picked object for production.
 def load_model():
     global model
-    model = hub.load('https://tfhub.dev/google/movenet/multipose/lightning/1')
+    model = hub.load('./movenet-model')
     model = model.signatures['serving_default'] # default model
 
 @app.route('/', methods=['GET'])
@@ -45,11 +45,12 @@ def index():
     username = auth.authenticate()
     given = flask.session.get('given_name')
     if model is None:
+        # no server error until this point...
         load_model()
-        html_code = flask.render_template('404.html', username=username,
-                                        given=given)
-        response = flask.make_response(html_code)
-        return response
+        #html_code = flask.render_template('404.html', username=username,
+                                     #   given=given)
+        #response = flask.make_response(html_code)
+        #return response
 
     html_code = flask.render_template('index.html', username=username,
                                     given=given)
